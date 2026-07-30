e9c1ffff86636b535b6db49d89530d130c7c3d80
Ansh Ahuja <169084965+Ansh-P1@users.noreply.github.com>
2026-07-30 23:37:50 +0530

    Time: 0 ms (100.00%) | Memory: 20.8 MB (77.85%) - LeetSync

--- 73-set-matrix-zeroes/set-matrix-zeroes.cpp
+++ 73-set-matrix-zeroes/set-matrix-zeroes.cpp
@@ -3,53 +3,22 @@ public:
     void setZeroes(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-
-        bool firstRowZero = false;
-        bool firstColZero = false;
-
-        // check if first row has any zero
-        for (int j = 0; j < n; j++){
-            if (matrix[0][j] == 0){
-                firstRowZero = true;
-                break;
-            }
-        }
-
-        // check if first column has any zero
-        for (int i = 0; i < m; i++){
-            if (matrix[i][0] == 0){
-                firstColZero = true;
-                break;
-            }
-        }
-
-        // use first row/col as markers for the rest of the matrix
-        for (int i = 1; i < m; i++){
-            for (int j = 1; j < n; j++){
-                if (matrix[i][j] == 0){
-                    matrix[i][0] = 0;
-                    matrix[0][j] = 0;
+        vector<int> row(m,0);
+        vector<int> col(n,0);
+        for (int i=0; i<m; i++){
+            for (int j =0; j<n; j++){
+                if (matrix[i][j] ==0){
+                    row [i] =1;
+                    col [j]= 1;
                 }
             }
         }
-
-        // zero out cells based on markers (skip first row/col for now)
-        for (int i = 1; i < m; i++){
-            for (int j = 1; j < n; j++){
-                if (matrix[i][0] == 0 || matrix[0][j] == 0){
+        for (int i =0; i<m; i++){
+            for (int j=0; j<n; j++){
+                if (row [i] || col[j]){
                     matrix[i][j] = 0;
                 }
             }
-        }
-
-        // handle first row
-        if (firstRowZero){
-            for (int j = 0; j < n; j++) matrix[0][j] = 0;
-        }
-
-        // handle first column
-        if (firstColZero){
-            for (int i = 0; i < m; i++) matrix[i][0] = 0;
         }
     }
 };

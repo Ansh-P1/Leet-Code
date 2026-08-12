5e6c9e4bd186da985bb0e2d72fd67cd364dcfd19
Ansh Ahuja <169084965+Ansh-P1@users.noreply.github.com>
2026-08-12 18:51:04 +0530

    Time: 83 ms (5.57%) | Memory: 72.5 MB (5.58%) - LeetSync

--- 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/find-all-duplicates-in-an-array.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     vector<int> findDuplicates(vector<int>& nums) {
-         unordered_set<int> st, dup; ///create two unordered maps one to store the singles and record them and other to check for duplicates 
-        for (int i : nums){
-            if (st.count(i)) dup.insert(i); ///count sees if an int 1 already exists in st or no, insert inserts it into the unordered set
+        unordered_set<int> st,dup;
+        for (int i: nums){
+            if (st.count(i)){
+                dup.insert(i);
+            }
             else st.insert(i);
         }
-        vector<int> a; 
-        for(int i :dup){
-            a.push_back(i);
+        vector<int> ans;
+        for (int i : dup){
+            ans.push_back(i);
         }
-        return a;
+        return ans; 
     }
 };
